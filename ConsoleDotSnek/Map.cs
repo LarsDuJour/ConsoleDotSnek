62b3a6d7339ff18c8b0d29ff5d67dd453f340423
Lars du Jour <lars66c1@edu.easj.dk>
2018-09-28 12:30:28 +0200

    Initial commit

---
+++ ConsoleDotSnek/Map.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotSnek
+{
+    class Map
+    {
+        private char[,] map;
+        private int mapWidth;
+        private int mapHeight;
+
+        public Map()
+        {
+            map = new char[10,10]
+            {
+                { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', '.', '.', '.', '.', '.', '.', '.', '.', 'x' },
+                { 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' }
+            };
+
+            mapHeight = 9;
+            mapWidth = 9;
+
+
+        }
+
+
+
+        public void printMap()
+        {
+            for (int i = 0; i <= mapWidth; i++)
+            {
+                for (int j = 0; j <= mapHeight; j++)
+                {
+                 Console.Write(map[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleDotSnek/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotSnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm just a poor console, nobody reads me");
+
+            Map gameMap = new Map();
+            gameMap.printMap();
+        }
+    }
+}
