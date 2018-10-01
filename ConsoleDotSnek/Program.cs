ef29cb510eb28b0e20b4ad9b1d3078450083016c
Lars du Jour <lars66c1@edu.easj.dk>
2018-10-01 19:20:41 +0200

    More functionality
    Now the head of the snake "moves"

--- ConsoleDotSnek/Program.cs
+++ ConsoleDotSnek/Program.cs
@@ -2,18 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleDotSnek
 {
     class Program
     {
+
+        
+
         static void Main(string[] args)
         {
+
+            bool isGamePlaying = true;
+
             Console.WriteLine("I'm just a poor console, nobody reads me");
 
             Map gameMap = new Map();
             gameMap.printMap();
+
+            Snake tempSnake = new Snake();
+
+
+            while (isGamePlaying)
+            {
+                Console.Clear();
+
+                tempSnake.updateLocation();
+                gameMap.updateMap(tempSnake); //currently only does one snake
+                gameMap.printMap();
+
+
+
+                Console.ReadLine();
+
+            }
+
+                
+
+
         }
     }
 }
---
+++ ConsoleDotSnek/Snake.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleDotSnek
+{
+    public class Snake
+    {
+        //initial location data for debugging
+        private int locX = 1;
+        private int prevLocX; //for storing previous location
+        private int locY = 1;
+        private int prevLocY; //for storing previous location
+
+        //initial movement velocity
+        private int velX = 0; //left and right
+        private int velY = 1; //up and down
+
+        //more info
+        public char snakeChar = 'O'; //show the snake using an O
+        //private int snakeLength = 1; //temp for setting the max length of an array
+
+
+
+
+        public Snake()
+        {
+            //empty for now
+        }
+
+
+        //for updating the location of the snake
+        public void updateLocation()
+        {
+            prevLocX = locX;
+            locX = locX + velX;
+
+            prevLocY = locY;
+            locY = locY + velY;
+        }
+
+
+
+        public int getSnakeLocX()
+        {
+            return locX;
+        }
+
+        public int getSnakeLocY()
+        {
+            return locY;
+        }
+
+
+        public int getPrevSnakeLocX()
+        {
+            return prevLocX;
+        }
+
+        public int getPrevSnakeLocY()
+        {
+            return prevLocY;
+        }
+
+    }
+}
